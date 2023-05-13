52ab449a7e497773e754e05f029e088b6e80fec6
Titotrapatonni <titovtelexfree@gmail.com>
2023-05-13 20:17:46 +0300

    change controllers export

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,26 @@ const express = require("express");
 
 const router = express.Router();
 
-const ctrl = require("../../controllers/contacts");
+const {
+  getAll,
+  getById,
+  add,
+  deleteById,
+  updateById,
+} = require("../../controllers/contacts");
 
 const { validateBody } = require("../../middlewares");
 
 const schemas = require("../../schemas");
 
-router.get("/", ctrl.getAll);
+router.get("/", getAll);
 
-router.get("/:contactId", ctrl.getById);
+router.get("/:contactId", getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.add);
+router.post("/", validateBody(schemas.addSchema), add);
 
-router.delete("/:contactId", ctrl.deleteById);
+router.delete("/:contactId", deleteById);
 
-router.put("/:contactId", validateBody(schemas.addSchema), ctrl.updateById);
+router.put("/:contactId", validateBody(schemas.addSchema), updateById);
 
 module.exports = router;
