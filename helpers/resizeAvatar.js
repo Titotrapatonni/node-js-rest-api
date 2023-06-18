070fcf5336d29aadc4232a2e701ddecb1d606cd3
Titotrapatonni <titovtelexfree@gmail.com>
2023-06-18 11:53:18 +0300

    change hlpr resizeAvatar

--- helpers/resizeAvatar.js
+++ helpers/resizeAvatar.js
@@ -2,7 +2,7 @@ const jimp = require("jimp");
 
 async function resizeAvatar(tempPath, resultPath) {
   const image = await jimp.read(tempPath);
-  await image.cover(250, 250);
+  await image.resize(250, 250);
   await image.writeAsync(resultPath);
 }
 
