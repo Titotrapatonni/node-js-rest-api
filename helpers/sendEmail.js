d558824280215bf27e402251a38ad3094726853f
Titotrapatonni <titovtelexfree@gmail.com>
2023-06-18 16:39:53 +0300

    clear comments

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -5,18 +5,6 @@ const { SG_API_KEY } = process.env;
 
 sgMail.setApiKey(SG_API_KEY);
 
-// const email = {
-//   to: "fijived657@bodeem.com",
-//   from: "titovtelexfree@gmail.com",
-//   subject: "Test email",
-//   html: "<p><strong>Test email</strong> from Eugene Titov</p>",
-// };
-
-// sgMail
-//   .send(email)
-//   .then(() => console.log("Send email succes"))
-//   .catch((error) => console.log(error.message));
-
 const sendEmail = async (data) => {
   const email = { ...data, from: "titovtelexfree@gmail.com" };
   await sgMail.send(email);
